33123d8ecafc037f95cceb881315df736a1e2478
Arnav Arvind <f20212709@hyderabad.bits-pilani.ac.in>
2023-03-29 22:51:05 +0530

    fixes

--- components/AdminComponents/Modal.js
+++ components/AdminComponents/Modal.js
@@ -1,10 +1,9 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useForm } from "react-hook-form";
-import { doc, getDoc,updateDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
-export default function Modal({ open, setOpen, user,setAlert }) {
-
+export default function Modal({ open, setOpen, user, setAlert }) {
   const {
     register,
     handleSubmit,
@@ -12,8 +11,8 @@ export default function Modal({ open, setOpen, user,setAlert }) {
     formState: { errors },
   } = useForm();
   const onSubmit = async (modalData) => {
-    setOpen(false)
-    
+    setOpen(false);
+
     try {
       const docRef = doc(db, "users", user.uid);
       const docSnap = await getDoc(docRef);
@@ -21,14 +20,11 @@ export default function Modal({ open, setOpen, user,setAlert }) {
       const country = urlSearchParams.get("country");
       const gameName = urlSearchParams.get("gameName");
 
-
       if (docSnap.exists()) {
-
         const data = docSnap.data();
         let gamePoints = 0;
         let gameHealth = 0;
         let gameCompleted = false;
-
 
         let temp = data.roomDetails.map((room) => {
           if (room.roomName == country) {
@@ -36,29 +32,31 @@ export default function Modal({ open, setOpen, user,setAlert }) {
             room.roomHealth += parseInt(modalData.health);
             room.games.map((game) => {
               if (game.name == gameName) {
-                game.points = parseInt(game.points) + parseInt(modalData.points);
-                gamePoints = game.points
-                game.health = parseInt(game.health) + parseInt(modalData.health);
-                gameHealth = game.health
+                game.points =
+                  parseInt(game.points) + parseInt(modalData.points);
+                gamePoints = game.points;
+                game.health =
+                  parseInt(game.health) + parseInt(modalData.health);
+                gameHealth = game.health;
                 game.completed = Boolean(modalData.completion);
-                gameCompleted = game.completed
+                gameCompleted = game.completed;
               }
               return game;
             });
           }
           return room;
         });
-        let totalPoints = parseInt(data.totalPoints) + parseInt(modalData.points);
-        let totalHealth = parseInt(data.totalHealth) + parseInt(modalData.health);
+        let totalPoints =
+          parseInt(data.totalPoints) + parseInt(modalData.points);
+        let totalHealth =
+          parseInt(data.totalHealth) + parseInt(modalData.health);
         await updateDoc(docRef, {
           roomDetails: temp,
-          totalHealth : totalHealth,
-          totalPoints : totalPoints,
-          currency : totalPoints * 0.5 * totalHealth
-        })
-        setAlert(gamePoints,gameHealth,gameCompleted)
-        
-
+          totalHealth: totalHealth,
+          totalPoints: totalPoints,
+          currency: totalPoints * 0.5 * totalHealth,
+        });
+        setAlert(gamePoints, gameHealth, gameCompleted);
       }
     } catch (error) {
       console.log(error);
@@ -69,105 +67,105 @@ export default function Modal({ open, setOpen, user,setAlert }) {
 
   return (
     <>
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setOpen}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  
-                  <div className="bg-white px-4 pb-3 px-6 justify-center">
-                  <div className=" py-6 grid gap-6 mb-6 flex flex-row justify-center">
-                          <div>
-                            <label
-                              htmlFor="points"
-                              className="text-base font-semibold leading-6 text-purple-800"
-                            >
-                              Add Points
-                            </label>
-                            <input
-                              id="points"
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2"
-                              {...register("points")}
-                            />
-                          </div>
-                          <div>
-                            <label
-                              htmlFor="health"
-                              className="text-base font-semibold leading-6 text-purple-800"
-                            >
-                              Add Health
-                            </label>
-                            <input
-                              id="health"
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2"
-                              {...register("health")}
-                            />
-                          </div>
-                          <label className="relative inline-flex items-center mr-5 cursor-pointer">
-                            <input
-                              type="checkbox"
-                              defaultValue
-                              className="sr-only peer"
-                              {...register("completion")}
-                            />
-                            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600" />
-                            <span className="text-base font-semibold leading-6 text-purple-800 mx-5">
-                              Completed
-                            </span>
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="bg-white px-4 pb-3 px-6 justify-center">
+                      <div className=" py-6 grid gap-6 mb-6 flex flex-row justify-center">
+                        <div>
+                          <label
+                            htmlFor="points"
+                            className="text-base font-semibold leading-6 text-purple-800"
+                          >
+                            Add Points
                           </label>
+                          <input
+                            id="points"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2"
+                            required
+                            {...register("points")}
+                          />
                         </div>
-    <div className="bg-white px-4 pb-3 px-6 flex flex-row-reverse justify-center">
-    <button
-                      type="button"
-                      className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-3 mb-2"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="submit"
-                      className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-2"
-                    >
-                      Submit
-                    </button>
-    </div>
-                    
-                  </div>
-                </form>
-              </Dialog.Panel>
-            </Transition.Child>
+                        <div>
+                          <label
+                            htmlFor="health"
+                            className="text-base font-semibold leading-6 text-purple-800"
+                          >
+                            Add Health
+                          </label>
+                          <input
+                            id="health"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2"
+                            required
+                            {...register("health")}
+                          />
+                        </div>
+                        <label className="relative inline-flex items-center mr-5 cursor-pointer">
+                          <input
+                            type="checkbox"
+                            defaultValue
+                            className="sr-only peer"
+                            {...register("completion")}
+                          />
+                          <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600" />
+                          <span className="text-base font-semibold leading-6 text-purple-800 mx-5">
+                            Completed
+                          </span>
+                        </label>
+                      </div>
+                      <div className="bg-white px-4 pb-3 px-6 flex flex-row-reverse justify-center">
+                        <button
+                          type="button"
+                          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-3 mb-2"
+                          onClick={() => setOpen(false)}
+                          ref={cancelButtonRef}
+                        >
+                          Cancel
+                        </button>
+                        <button
+                          type="submit"
+                          className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-2"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 }
